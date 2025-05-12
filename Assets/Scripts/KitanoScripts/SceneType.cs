c8c5c4236ca86e684572b9b416aa13e62b60e25b
PL2401A1217-W\PL2410A1217 <nakajima_yu@1923.co.jp>
2025-05-12 15:38:53 +0900

    TitleシーンからLobbyシーンに移動する機能を更新

--- Assets/Scripts/KitanoScripts/SceneType.cs
+++ Assets/Scripts/KitanoScripts/SceneType.cs
@@ -6,8 +6,7 @@ public enum SceneType
     Lobby,
     Battle,
     Result,
-    Test_Loaded,
-    Sample
+    Test_Loaded
 }
 
 public static class SceneTypeExtensions
@@ -24,7 +23,6 @@ public static class SceneTypeExtensions
             SceneType.Battle => "BattleScene",
             SceneType.Result => "ResultScene",
             SceneType.Test_Loaded => "SceneLoadTest_Loaded",
-            SceneType.Sample => "SampleScene",
             _ => throw new ArgumentOutOfRangeException(nameof(type), $"–¢’è‹`‚ÌSceneType‚Å‚·: {type}")
         };
     }
