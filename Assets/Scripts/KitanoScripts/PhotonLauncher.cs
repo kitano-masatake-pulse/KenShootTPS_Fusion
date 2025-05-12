a3e1ae5a3c95db2eba2c6baaf218c53d4c719a4a
PL2409A1215-w\PL2409A1215 <kitano_m@1923.co.jp>
2025-05-12 15:01:42 +0900

    fusion用のPhotonLauncher作成

---
+++ Assets/Scripts/KitanoScripts/PhotonLauncher.cs
@@ -0,0 +1,41 @@
+using System;
+ using System.Collections.Generic;
+  using Fusion;
+ using Fusion.Sockets;
+  using UnityEngine;
+
+ // INetworkRunnerCallbacksを実装して、NetworkRunnerのコールバック処理を実行できるようにする
+ public class PhotonLauncher : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [SerializeField]
+    private NetworkRunner networkRunnerPrefab;
+
+    private NetworkRunner networkRunner;
+
+    private async void Start()
+    {
+        networkRunner = Instantiate(networkRunnerPrefab);
+              // NetworkRunnerのコールバック対象に、このスクリプト（GameLauncher）を登録する
+        networkRunner.AddCallbacks(this);
+        var result = await networkRunner.StartGame(new StartGameArgs
+        {
+            GameMode = GameMode.AutoHostOrClient,
+
+            SceneManager = networkRunner.GetComponent<NetworkSceneManagerDefault>()
+        });
+    }
+        
+     // INetworkRunnerCallbacksインターフェースの空実装
+     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+     public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner) { }     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data) { }     public void OnSceneLoadDone(NetworkRunner runner) { }
+     public void OnSceneLoadStart(NetworkRunner runner) { }
+}
