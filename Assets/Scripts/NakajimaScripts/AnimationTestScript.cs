de50c6a47ab54ff16438d59700d6c67d4051956a
PL2401A1217-W\PL2410A1217 <nakajima_yu@1923.co.jp>
2025-05-16 15:05:51 +0900

    アニメーション遷移テスト作成 前後ろ左右。ジャンプ作成。

--- Assets/Scripts/NakajimaScripts/AnimationTestScript.cs
+++ Assets/Scripts/NakajimaScripts/AnimationTestScript.cs
@@ -9,7 +9,8 @@ public class AnimationTestScript : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 8f;
-    public float gravity = 20f;
+    public float gravity = 9.81f;
+    private bool isJumping = false;
 
     private CharacterController controller;
     private Vector3 moveDirection;
@@ -21,42 +22,45 @@ public class AnimationTestScript : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
     }
 
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 input = new Vector3(horizontal, 0, vertical);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 input = new Vector3(h, 0, v);
         input = Vector3.ClampMagnitude(input, 1f);
 
-        Vector3 movement = transform.TransformDirection(input) * moveSpeed;
+        Vector3 move = transform.TransformDirection(input) * moveSpeed;
+        move.y = moveDirection.y;
 
         if (controller.isGrounded)
         {
-            moveDirection = movement;
+            if (isJumping)
+            {
+                isJumping = false;
+            }
+
+            move.y = -1f;
 
             if (Input.GetButtonDown("Jump"))
             {
-                moveDirection.y = jumpForce;
-                animator.SetBool("IsJumping", true);
-            }
-            else
-            {
-                animator.SetBool("IsJumping", false);
+                move.y = jumpForce;
+                isJumping = true;
             }
         }
         else
         {
-            // 空中
-            animator.SetBool("IsJumping", true);
-            moveDirection.x = movement.x;
-            moveDirection.z = movement.z;
+            move.y -= gravity * Time.deltaTime;
         }
 
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        controller.Move(move * Time.deltaTime);
+        moveDirection = move;
 
-        // アニメーション用Speed更新
-        float speed = new Vector3(controller.velocity.x, 0, controller.velocity.z).magnitude;
-        animator.SetFloat("Speed", speed);
+        // Animator 更新
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.z);
+        animator.SetBool("IsJumping", isJumping);
+        animator.SetFloat("YVelocity", move.y);
     }
+
 }
