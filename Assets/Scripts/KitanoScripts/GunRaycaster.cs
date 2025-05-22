e1cb5ed996a069766f92e1295156a2740e4eeb20
PL2409A1213-w\PL2409A1213 <kitano_m@1923.co.jp>
2025-05-22 16:49:02 +0900

    hitboxを使った攻撃(テスト用)を実装

---
+++ Assets/Scripts/KitanoScripts/GunRaycaster.cs
@@ -0,0 +1,116 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRaycaster : NetworkBehaviour
+{
+
+    public TPSCameraController playerCamera; // 自分のTPSカメラ
+    public float fireDistance = 100f;
+    public LayerMask hitMask;
+
+    public int weaponDamage = 10;
+
+
+    public override void Spawned()
+    {
+
+        playerCamera = FindObjectOfType<TPSCameraController>();
+
+    }
+
+
+        // Start is called before the first frame update
+        void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Gun Fire!");
+            GunFire(playerCamera.transform.position, playerCamera.transform.forward);
+            
+
+         }
+
+
+    }
+
+    //originからdirection方向へ打つ
+    public void GunFire(Vector3 origin, Vector3 direction)
+    {
+
+        Runner.LagCompensation.Raycast(
+            origin,
+            direction,
+            fireDistance,
+            Object.InputAuthority,
+            out var hit,
+            hitMask.value, //判定を行うレイヤーを制限する
+            HitOptions.None);
+
+        Debug.DrawRay(
+            origin,
+            direction * fireDistance,
+            Color.red, 1f);
+
+
+        Debug.Log("Hit?" + hit.GameObject);
+        //着弾処理 
+        if (hit.GameObject != null)
+        {
+            Debug.Log("Hit!" + hit.GameObject);
+
+            //当たった対象がPlayerHitboxを持っていたらダメージ処理
+            if (hit.Hitbox is PlayerHitbox playerHitbox)
+            {
+
+                CauseDamage(hit, weaponDamage);
+            }
+            else
+            {
+                Debug.Log("Hit! but not Player");
+            }
+
+
+        }
+        
+
+    }
+
+    void CauseDamage(LagCompensatedHit hit, int weaponDamage)
+    {
+
+        //当たった対象がPlayerHitboxを持っていたらダメージ処理
+        if (hit.Hitbox is PlayerHitbox playerHitbox)
+        {
+            PlayerRef targetPlayerRef = playerHitbox.myPlayerRef;
+            Debug.Log($" Get playerHitbox,PlayerRef :{targetPlayerRef} ");
+            RPC_RequestDamage(targetPlayerRef, weaponDamage);
+        }
+        else
+
+        {
+            Debug.Log($"Couldn't Get playerHitbox, but{hit.Hitbox} ");
+        }
+
+    }
+
+
+    //自作クラスを引数にとっても大丈夫？→重いのでダメ
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_RequestDamage(PlayerRef targetPlayerRef, int Damage)
+    {
+        PlayerAvatar damagedPlayerAvatarScript = Runner.GetPlayerObject(targetPlayerRef).GetComponent<PlayerAvatar>();
+
+        damagedPlayerAvatarScript.TakeDamage(Damage);
+
+
+    }
+}
