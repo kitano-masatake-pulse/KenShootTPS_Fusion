26a70e450a5be1912cd2016e811c24bc3d83e8ec
PL2401A1217-W\PL2410A1217 <nakajima_yu@1923.co.jp>
2025-07-09 11:38:51 +0900

    投擲完了になってないとグレネードが生成されない機能 アニメーションの遷移で判定している

--- Assets/Scripts/NakajimaScripts/GrenadeThrower.cs
+++ Assets/Scripts/NakajimaScripts/GrenadeThrower.cs
@@ -20,32 +20,14 @@ public class GrenadeThrower : MonoBehaviour
     Vector3 velocity;
 
     // 投擲準備中かどうかのフラグ
-    private bool isAiming;
+    private bool isAimingReady;
+    private bool hasEnterThrowOnce;
     void Update()
     {
-        // マウスの左ボタンが押されたら投擲準備を開始
-        if (Input.GetMouseButtonDown(0))
-        {
-            isAiming = true;
-        }
 
-        // マウスの左ボタンが押されている間の処理
-        if (Input.GetMouseButton(0) && isAiming)
-        {
-        }
-
-        // マウスの左ボタンが離された時の処理
-        if (Input.GetMouseButtonUp(0) && isAiming)
-        {
-            Debug.Log("Throwing grenade...");
-            // グレネードを投げる
-            ThrowGrenade(velocity);
-            isAiming = false;
-            // 軌跡を非表示にする
-            trajectoryDrawer.HideTrajectory();
-        }
-        // 投擲準備完了モーションの間LineRendererを表示する
-        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Prepare to throw loop") && isAiming)
+        // 投擲準備完了モーションの間LineRendererを表示
+        // ボタン離した後でも、アニメーションは少し続くので、フラグ判定挿入
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Prepare to throw loop"))
         {
             Debug.Log("Throwing grenade Loop...");
             //　投げる向きを取得する
@@ -55,7 +37,37 @@ public class GrenadeThrower : MonoBehaviour
 
             // 関数呼び出し（投げる位置,投げる向きと大きさ）
             trajectoryDrawer.RaycastDrawTrajectory(throwPoint.position, velocity);
+            isAimingReady = true;
         }
+
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Throw"))
+        {
+            if (!hasEnterThrowOnce)
+            {
+                // グレネードを投げる
+                ThrowGrenade(velocity);
+                // 軌跡を非表示にする
+                trajectoryDrawer.HideTrajectory();
+                hasEnterThrowOnce = true;
+                isAimingReady = false;
+            }
+        }
+        else
+        {
+            hasEnterThrowOnce = false;
+        }
+
+        // マウスの左ボタンが離された時の処理
+        if (Input.GetMouseButtonUp(0) && isAimingReady)
+        {
+            //// Updateメソッド内のDebug.Log行を以下のように変更
+            //Debug.Log($"Throwing grenade... isAimingReady: {isAimingReady}");
+            //// グレネードを投げる
+            //ThrowGrenade(velocity);
+            //// 軌跡を非表示にする
+            //trajectoryDrawer.HideTrajectory();
+        }
+
     }
 
     void ThrowGrenade(Vector3 velocity)
